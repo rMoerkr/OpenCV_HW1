2ea47cbf119c6dcd533ab82fbce5419722fc9f5c
MoerkFang <Moerk@live.cn>
2018-10-05 16:11:02 +0800

    完成作业4
    读懂了作业4并完成
    修正了作业3计算方式错误的BUG
    删除了容易出错的不必要输出

--- OpenCV_Edge/main.cpp
+++ OpenCV_Edge/main.cpp
@@ -13,9 +13,11 @@ int main() {
 	cin >> level;
 	Mat pic(400, 500, CV_8UC3, Scalar(0, 0, 255));
 	Mat picPlus(400, 500, CV_8UC3, Scalar(0, 0, 255));
-	Mat picMine(400, 500, CV_8UC3, Scalar(0, 0, 255));
+	Mat picMinus(400, 500, CV_8UC3, Scalar(0, 0, 255));
 	Mat picLevel(400, 500, CV_8UC3, Scalar(0, 0, 255));
-	vector<int[3]> vec(level);
+	Mat picLevel2(400, 500, CV_8UC3, Scalar(0, 0, 255));
+	vector<int[3]> vec(level);//儲存出現頻率的向量
+	int sum[3] = { 0,0,0 };
 	int BGR[3][4][5] = {
 		{ {17,44,32,127,240},	{22,231,21,156,124},	{32,35,78,21,194},		{127,22,33,54,45} } ,
 		{ {14,145,132,25,40},	{212,221,121,54,14},	{132,235,178,19,14} ,	{122,122,133,54,47} } ,
@@ -29,11 +31,24 @@ int main() {
 					for (int jm = 0; jm < 100; jm++) {
 						pic.at<cv::Vec3b>(i * 100 + im, j * 100 + jm)[k] = BGR[k][i][j];
 						picPlus.at<cv::Vec3b>(i * 100 + im, j * 100 + jm)[k] = min(BGR[k][i][j] + 32, 255);
-						picMine.at<cv::Vec3b>(i * 100 + im, j * 100 + jm)[k] = max(BGR[k][i][j] - 20, 0);
-						picLevel.at<cv::Vec3b>(i * 100 + im, j * 100 + jm)[k] = (int)((BGR[k][i][j] / level) * (50));
+						picMinus.at<cv::Vec3b>(i * 100 + im, j * 100 + jm)[k] = max(BGR[k][i][j] - 20, 0);
+						picLevel.at<cv::Vec3b>(i * 100 + im, j * 100 + jm)[k] = ((int)(BGR[k][i][j] / (255 / level))) * (255/level);
 					}
 				}
 				vec[(int)((BGR[k][i][j] / (255 / level)))][k]++;
+				sum[k]++;
+			}
+		}
+	}
+	for (int i = 0; i < pic.rows / 100; i++) {
+		for (int j = 0; j < pic.cols / 100; j++) {
+			for (int k = 0; k < 3; k++) {//顔色空間
+				for (int im = 0; im < 100; im++) {
+					for (int jm = 0; jm < 100; jm++) {
+						picLevel2.at<cv::Vec3b>(i * 100 + im, j * 100 + jm)[k] = 
+							((vec[((int)picLevel.at<cv::Vec3b>(i * 100, j * 100)[k] / (255 / level))][k]) *255 /sum[k]);
+					}
+				}
 			}
 		}
 	}
@@ -43,13 +58,10 @@ int main() {
 		cout << "    G" << vec[i][1] << endl;
 		cout << "    R" << vec[i][2] << endl;
 	}
-	imwrite("pic.jpg", pic);
-	imwrite("picPlus.jpg", picPlus);
-	imwrite("picMine.jpg", picMine);
-	imwrite("picLevel.jpg", picLevel);
-	imshow("pic", pic);
-	imshow("picPlus", picPlus);
-	imshow("picMine", picMine);
-	imshow("picLevel", picLevel);
-	waitKey(0);
+	cv::imshow("pic", pic);
+	cv::imshow("picPlus", picPlus);
+	cv::imshow("picMinus", picMinus);
+	cv::imshow("picLevel", picLevel);
+	cv::imshow("picLevel2", picLevel2);
+	cv::waitKey(0);
 }
